d2c0039ef05ba00a157e79c09c65b84e148eaa9b
MikaelRothig <mrrothig@gmail.com>
2023-02-02 18:04:47 +0100

    Added new files and unpdated index.js

--- index.js
+++ index.js
@@ -4,17 +4,35 @@ const Discord = require("discord.js");
 require("dotenv").config();
 
 const client = new Discord.Client({
-  intents: ["Guilds", "GuildMessages", "MessageContent"],
+  intents: ["Guilds", "GuildMessages", "MessageContent", "GuildMembers"],
 });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+let bot = {
+  client,
+  prefix: "n.",
+  owners: ["265193428931837952"],
+};
 
-client.on("messageCreate", (message) => {
-  if (message.content == "hi") {
-    message.reply("Hello World!");
-  }
-});
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
+client.loadCommands = (bot, reload) =>
+  require("./handlers/commands")(bot, reload);
+
+client.loadEvents(bot, false);
+client.loadCommands(bot, false);
+
+module.exports = bot;
+
+// client.on("ready", () => {
+//   console.log(`Logged in as ${client.user.tag}`);
+// });
+
+// client.on("messageCreate", (message) => {
+//   if (message.content == "hi") {
+//     message.reply("Hello World!");
+//   }
+// });
 
 client.login(process.env.TOKEN);
